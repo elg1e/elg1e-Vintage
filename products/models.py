0c1e95b929b1da1655b5c9f90ff92d1b857b1a43
Dominic Elgie <domelgie94@gmail.com>
2020-04-06 22:32:23 +0000

    edits to products app

--- products/models.py
+++ products/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=250, default='')
     GENDER_CHOICES = [
         ('mens','mens'),
         ('womens','womens')
     ]
-    gender = models.CharField(choices=GENDER_CHOICES, default='mens')
+    name = models.CharField(max_length=250)
+    gender = models.CharField(max_length=50, choices=GENDER_CHOICES, default='mens')
     CLOTHING_TYPE = [
         ('tops','tops'),
         ('shirts','shirts'),
         ('jumpers','jumpers'),
         ('jeans','jeans'),
-        ('accessories','accessories'),
+        ('accessories','accessories')
     ]
-    clothingType = models.CharField(choices='CLOTHING_TYPE', default='')
+    clothingType = models.CharField(max_length=50, choices=CLOTHING_TYPE, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
