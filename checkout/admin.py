d7654f6a794e9ec8aec1b6fa04e40bbdffbc41d6
Dominic Elgie <domelgie94@gmail.com>
2020-04-17 10:29:36 +0000

    created data for checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Order, OrderLineitem
 
 # Register your models here.
+
+class OrderLineAdminInLine(admin.TabularInline):
+    model = OrderLineitem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineAdminInLine, )
+
+admin.site.register(Order, OrderAdmin)
