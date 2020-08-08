3848acea2264ae3ea4bcd1256bbbd4a6d79a55ab
Dominic Elgie <domelgie94@gmail.com>
2020-08-08 20:02:16 +0000

    changes made with productinfo creation

--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
+    largeDescription = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     buyout = models.DecimalField(max_digits=10, decimal_places=2)
     auctionTimer = models.DateTimeField(null=True)
