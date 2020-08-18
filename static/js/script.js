6b9c74ca8ce7438de75962830afbcf682f050a21
Dominic Elgie <domelgie94@gmail.com>
2020-08-18 22:33:54 +0000

    greeting script/edits to base.html

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+function loginFunction() {
+    var newDate = new Date();
+    var time = newDate.getHours();
+    var welcomeGreeting;
+
+    if (time < 12) {
+        welcomeGreeting = "Good Morning!";
+    } else if (time < 17) {
+        welcomeGreeting = "Good Afternoon!";
+    } else {
+        welcomeGreeting = "Good Evening!";
+    }
+
+    document.getElementById('greeting').innerHTML = welcomeGreeting;
+}
+
+window.onload = function() {loginFunction();}
