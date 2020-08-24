5ae7b9fb8e1dccc81d300f92f02755351be94174
Dominic Elgie <domelgie94@gmail.com>
2020-08-24 12:15:03 +0000

    comments/edits to html

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,19 @@
 function loginFunction() {
-    var newDate = new Date();
-    var time = newDate.getHours();
-    var welcomeGreeting;
+	var newDate = new Date();
+	var time = newDate.getHours();
+	var welcomeGreeting;
 
-    if (time < 12) {
-        welcomeGreeting = "Good Morning!";
-    } else if (time < 17) {
-        welcomeGreeting = "Good Afternoon!";
-    } else {
-        welcomeGreeting = "Good Evening!";
-    }
+	if (time < 12) {
+		welcomeGreeting = "Good Morning!";
+	} else if (time < 17) {
+		welcomeGreeting = "Good Afternoon!";
+	} else {
+		welcomeGreeting = "Good Evening!";
+	}
 
-    document.getElementById('greeting').innerHTML = welcomeGreeting;
+	document.getElementById('greeting').innerHTML = welcomeGreeting;
 }
 
-window.onload = function() {loginFunction();}
+window.onload = function() {
+	loginFunction();
+};
