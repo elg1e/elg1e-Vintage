677717da76477e2264d6610a72f0cb815e1c505b
Dominic Elgie <domelgie94@gmail.com>
2020-08-30 20:06:54 +0000

    DEBUG OFF

--- vintage/settings.py
+++ vintage/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRECT_KEY', "Env value not loaded")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["vintage-fullstack.herokuapp.com", "localhost"]
 
